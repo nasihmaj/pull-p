bc3ef7e7114e5bac08ad67015887eae6f04d3680
info <nasihz12345@gmail.com>
2023-12-08 19:19:01 -0500

    "project files added"

---
+++ pull-p/src/test/java/com/nas/pullp/PullPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nas.pullp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

95fde2e963b9fe6b326c9bbb3613bdd7dbc84351
info <nasihz12345@gmail.com>
2023-12-08 20:36:33 -0500

    2nd method

--- pull-p/src/main/java/com/nas/pullp/PullPApplication.java
+++ pull-p/src/main/java/com/nas/pullp/PullPApplication.java
@@ -9,6 +9,10 @@ public class PullPApplication {
 	public void add() {
 		System.out.println("hh");
 	}
+
+	public void add2(){
+		System.out.println("qq");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PullPApplication.class, args);
 	}
